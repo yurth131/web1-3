d9c86258b088d6b5f14f656fdebe2ba48c43ab6a
Unknown <g1727073@st.t-kougei.ac.jp>
2018-05-07 17:46:02 +0900

    05/07　最初のコミット

--- Sample/Assets/Test.cs
+++ Sample/Assets/Test.cs
@@ -2,6 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Player
+{
+    private int hp = 100;
+    private int power = 50;
+
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+    public void Damage(int damage)
+    {
+        this.hp -= damage;
+        Debug.Log(damage + "のダメージを受けた");
+    }
+}
+
 public class Test : MonoBehaviour {
 
 	// Use this for initialization
@@ -115,8 +131,70 @@ public class Test : MonoBehaviour {
             
         }Debug.Log(sum);
 
+        int[] array = new int[5];
+
+        array[0] = 2;
+        array[1] = 10;
+        array[2] = 5;
+        array[3] = 15;
+        array[4] = 3;
+
+        for(int i = 0; i < 5; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+        int[] points = { 83, 99, 52, 93, 15 };
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            if (points[i] >= 90)
+            {
+                Debug.Log(points[i]);
+            }
+        }
+
+        SayHello();
+
+        CallName("Tom");
+
+        int answer1;
+        answer1 = Add(2, 3);
+        Debug.Log(answer1);
+
+        Player myPlayer = new Player();
+        myPlayer.Attack();
+        myPlayer.Damage(30);
+
+        Vector2 playerPos = new Vector2(3.0f, 4.0f);
+        playerPos.x += 8.0f;
+        playerPos.y += 5.0f;
+        Debug.Log(playerPos);
+
+        Vector2 startPos = new Vector2(2.0f, 1.0f);
+        Vector2 endPos = new Vector2(8.0f, 5.0f);
+        Vector2 dir = endPos - startPos;
+        Debug.Log(dir);
+
+        float len = dir.magnitude;
+        Debug.Log(len);
+
     }
-	
+    void SayHello()
+        {
+            Debug.Log("Hello");
+        }
+
+    void CallName(string name)
+    {
+        Debug.Log("Hello" + name);
+    }
+
+    int Add(int a,int b)
+    {
+        int c = a + b;
+        return c;
+    }
 	// Update is called once per frame
 	void Update () {
 		
